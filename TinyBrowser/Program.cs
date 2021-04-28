2c799cf7d3809af6d5c0fdfb239dbaff8a7ca5d5
Linus Å <71346418+LynkanRex@users.noreply.github.com>
2021-04-28 11:52:33 +0200

    Adds links as list via regex
    Courtesy of https://yetanotherchris.dev/csharp/extracting-all-links-from-a-html-page/

--- TinyBrowser/Program.cs
+++ TinyBrowser/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net.Sockets;
+using System.Text.RegularExpressions;
 
 namespace TinyBrowser
 {
@@ -27,8 +29,25 @@ namespace TinyBrowser
 
             string response = reader.ReadToEnd();
             
-            Console.Write(response);
+            //Console.Write(response);
 
+            int first = response.IndexOf("<body>");
+            int last = response.IndexOf("</body>");
+            
+            string bodySubstring = response.Substring(first, last - first);
+            
+            //Console.Write(bodySubstring);
+
+            List<string> linksList = LinkExtractor.Extract(bodySubstring);
+            var index = 0;
+
+            foreach (var link in linksList)
+            {
+                index++;
+                Console.WriteLine(index + " - " + link);
+            }
+            
+            Console.WriteLine("Make a selection");
             var choice = Console.ReadLine();
             if (choice == "Exit")
             {
@@ -37,6 +56,28 @@ namespace TinyBrowser
                 reader.Close();
                 tcpClient.Close();
             }
+        }
+    }
+
+    class LinkExtractor
+    {
+        public static List<string> list = new List<string>();
+
+        public static List<string> Extract(string html)
+        {
+            List<string> list = new List<string>();
+            
+            Regex regex = new Regex("(?:href|src)=[\"|']?(.*?)[\"|'|>]+", RegexOptions.Singleline | RegexOptions.CultureInvariant);
+
+            if (regex.IsMatch(html))
+            {
+                foreach (Match match in regex.Matches(html))
+                {
+                    list.Add(match.Groups[1].Value);
+                }
+            }
+            
+            return list;
         }
     }
 }
