2e141cef085034c3d5bf9bf36cfa600607fe071a
Linus Å <71346418+LynkanRex@users.noreply.github.com>
2021-04-29 11:55:38 +0200

    Allows choice input to navigate to another link

--- TinyBrowser/Program.cs
+++ TinyBrowser/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
 using System.IO;
 using System.Net.Sockets;
 using System.Text.RegularExpressions;
@@ -11,73 +13,131 @@ namespace TinyBrowser
 
         private static TcpClient tcpClient;
         private static Stream stream;
+
+        private static string host = "acme.com";
+        private static string originPath = "/";
         
         static void Main(string[] args)
         {
             tcpClient = new TcpClient();
-            tcpClient.Connect("acme.com", 80);
+            tcpClient.Connect(host, 80);
             tcpClient.SendTimeout = 3000;
             tcpClient.ReceiveTimeout = 3000;
             
             StreamWriter writer = new StreamWriter(tcpClient.GetStream());
             StreamReader reader = new StreamReader(tcpClient.GetStream());
-
-            writer.WriteLine("GET / HTTP/1.1");
-            writer.WriteLine("Host: www.acme.com");
-            writer.WriteLine("");
-            writer.Flush();
+            
+            string path = originPath;
+            
+            CompileWriterRequest(writer, path);
 
             string response = reader.ReadToEnd();
             
-            //Console.Write(response);
-
             int first = response.IndexOf("<body>");
             int last = response.IndexOf("</body>");
             
             string bodySubstring = response.Substring(first, last - first);
             
-            //Console.Write(bodySubstring);
-
-            List<string> linksList = LinkExtractor.Extract(bodySubstring);
-            var index = 0;
+            Dictionary<int, string> linksList = LinkExtractor.Extract(bodySubstring);
 
             foreach (var link in linksList)
             {
-                index++;
-                Console.WriteLine(index + " - " + link);
+                Console.WriteLine(link);
             }
+
+            bool activeSession = true;
             
-            Console.WriteLine("Make a selection");
-            var choice = Console.ReadLine();
-            if (choice == "Exit")
+            while (true)
             {
-                Console.WriteLine("Closing connection!");
-                writer.Close();
-                reader.Close();
-                tcpClient.Close();
+                Console.WriteLine("Make a selection, or enter 'Exit' to close");
+                
+                var choice = Console.ReadLine();
+                var convertedChoice = Convert.ToInt32(choice);
+            
+                if (choice == "Exit")
+                {
+                    Console.WriteLine("Closing connection!");
+                    writer.Close();
+                    reader.Close();
+                    tcpClient.Close();
+                    activeSession = false;
+                }
+                else
+                {
+                    if (Convert.ToInt32(choice) >= 0)
+                    {
+                        string result = "";
+                        if(linksList.TryGetValue(convertedChoice, out result))
+                        {
+                            path = "/"+result;
+                            
+                            Console.WriteLine($"Attempting to connect to {host}{path}");
+                            
+                            tcpClient.Close();
+                            tcpClient = new TcpClient();
+                            tcpClient.Connect(host, 80);
+                            
+                            
+                            writer = new StreamWriter(tcpClient.GetStream());
+                            reader = new StreamReader(tcpClient.GetStream());
+                            
+                            CompileWriterRequest(writer, path);
+                            
+                            string newResponse = reader.ReadToEnd();
+                            
+                            Console.Write(newResponse);
+                            int newFirst = newResponse.IndexOf("<body>");
+                            int newLast = newResponse.IndexOf("</body>");
+                            
+                            Console.WriteLine(newFirst);
+                            Console.WriteLine(newLast);
+                            
+                            string newBodySubstring = newResponse.Substring(newFirst+1, newLast - newFirst);
+            
+                            Dictionary<int, string> newLinksList = LinkExtractor.Extract(newBodySubstring);
+
+                            foreach (var link in newLinksList)
+                            {
+                                Console.WriteLine(link);
+                            }
+                            return;
+                        }
+                        Console.Write("Incorrect entry, please give a number and press Return");
+                    }
+                }
             }
+        }
+
+        private static void CompileWriterRequest(StreamWriter writer, string path)
+        {
+            Console.WriteLine($"GET {path} HTTP/1.1");
+            writer.WriteLine($"GET {path} HTTP/1.1");
+            writer.WriteLine("Host: www.acme.com");
+            writer.WriteLine("Connection: close");
+            writer.WriteLine("");
+            writer.Flush();
         }
     }
 
     class LinkExtractor
     {
-        public static List<string> list = new List<string>();
+        public static Dictionary<int, string> dictionary = new Dictionary<int, string>();
 
-        public static List<string> Extract(string html)
+        public static Dictionary<int, string> Extract(string html)
         {
-            List<string> list = new List<string>();
-            
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            int index = 1;            
             Regex regex = new Regex("(?:href|src)=[\"|']?(.*?)[\"|'|>]+", RegexOptions.Singleline | RegexOptions.CultureInvariant);
 
             if (regex.IsMatch(html))
             {
                 foreach (Match match in regex.Matches(html))
                 {
-                    list.Add(match.Groups[1].Value);
+                    dictionary.Add(index, match.Groups[1].Value);
+                    index++;
                 }
             }
-            
-            return list;
+            return dictionary;
         }
     }
 }
