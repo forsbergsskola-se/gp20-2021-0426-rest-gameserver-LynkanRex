b3d8b6ed3c201131d562b9e00cce43be8cc605ca
Linus Å <71346418+LynkanRex@users.noreply.github.com>
2021-04-29 15:32:38 +0200

    Adds secrets.json management

--- GitHubExplorer/Program.cs
+++ GitHubExplorer/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.IO;
+using System.Text.Json;
 
 namespace GitHubExplorer
 {
+    class Secrets
+    {
+        public string token { get; set; }
+    }
+    
     class Program
     {
+        static Secrets LoadAndValidateSecrets()
+        {
+            Secrets secrets;
+
+            if (!File.Exists("secrets.json"))
+            {
+                secrets = new Secrets();
+                File.WriteAllText("secrets.json", JsonSerializer.Serialize(secrets));
+            }
+            else
+            {
+                secrets = JsonSerializer.Deserialize<Secrets>(File.ReadAllText("secrets.json"));
+            }
+
+            if (string.IsNullOrEmpty(secrets.token))
+            {
+                throw new Exception("ERROR: You need to define a Token in 'secrets.json' file to work");
+            }
+            
+            return secrets;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var secret = LoadAndValidateSecrets();
+            
+            Console.WriteLine(secret.token);
+            
+            return;
+            Console.WriteLine("Hello! Please enter a username to view");
         }
     }
 }
