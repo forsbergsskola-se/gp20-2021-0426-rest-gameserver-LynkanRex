a4a45ef94a30a747957dca3985bcf1b406503d60
Linus Å <71346418+LynkanRex@users.noreply.github.com>
2021-04-30 16:08:48 +0200

    Allows extensibility for some other options

--- GitHubExplorer/Program.cs
+++ GitHubExplorer/Program.cs
@@ -44,6 +44,11 @@ namespace GitHubExplorer
             return splitString;
         }
 
+        private static void RunLoop()
+        {
+            
+        }
+
         static async Task Main(string[] args)
         {
             var secret = LoadAndValidateSecrets();
@@ -52,34 +57,53 @@ namespace GitHubExplorer
             HttpClient.DefaultRequestHeaders.UserAgent.Add(new System.Net.Http.Headers.ProductInfoHeaderValue("AppName", "1.0"));
             HttpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Token",secret.token);
 
-            bool sessionActive = true;
+            Console.WriteLine("Welcome to the GitHub explorer.");
 
-            while (true)
+            bool sessionActive = true;
+            
+            while (sessionActive)
             {
                 try
                 {
-                    Console.WriteLine("Welcome to the GitHub explorer. Please enter a username that you would like to have a look at:\nOr enter \"Exit\" to close");
+                    Console.WriteLine("Please enter either of the following options:\n" +
+                                      "users\n" +
+                                      "repos\n" +
+                                      "\nOr Exit to close");
+
                     var userChoice = Console.ReadLine();
+                    
+                    if (userChoice.ToLower() == "users")
+                    {
+                        // TODO: While-loop in choices as well to retain the user state, so they can look at other users and repos etc.
+                        Console.WriteLine("Please enter the name of the User that you'd like to look at");
+                        var userName = Console.ReadLine();
+
+                        Console.WriteLine($"Attempting to access {HttpClient.BaseAddress}users/{userName}");
+
+                        HttpResponseMessage response = await HttpClient.GetAsync($"users/{userName}");
+
+                        response.EnsureSuccessStatusCode();
+
+                        string responseBody = await response.Content.ReadAsStringAsync();
+
+                        var splitStrings = responseBody.SplitString();
+
+                        foreach (var entry in splitStrings)
+                        {
+                            Console.WriteLine(entry);
+                        }
+                    }
+
+                    if (userChoice.ToLower() == "repos")
+                    {
+                        Console.WriteLine("Repos it is");
+                    }
 
                     if (userChoice.ToLower() == "exit")
                     {
                         HttpClient.Dispose();
-                        break;
-                    }
-                    
-                    Console.WriteLine($"Attempting to access {HttpClient.BaseAddress}users/{userChoice}");
-                
-                    HttpResponseMessage response = await HttpClient.GetAsync($"users/{userChoice}");
-
-                    response.EnsureSuccessStatusCode();
-
-                    string responseBody = await response.Content.ReadAsStringAsync();
-                
-                    var splitStrings = responseBody.SplitString();
-
-                    foreach (var entry in splitStrings)
-                    {
-                        Console.WriteLine(entry);
+                        Console.WriteLine("Exiting...");
+                        sessionActive = false;
                     }
                 }
                 catch (HttpRequestException e)
@@ -87,6 +111,10 @@ namespace GitHubExplorer
                     Console.WriteLine("\nException caught!");
                     Console.WriteLine("\nMessage: {0} ", e.Message);
                     throw;
+                }
+                finally
+                {
+                    Console.WriteLine("Exited successfully");
                 }
             }
         }
